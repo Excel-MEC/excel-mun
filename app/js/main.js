cebf86c721445a43a76505750087ab9b2723ef20
Navin Mohan <navinmohan81@gmail.com>
2017-07-23 17:53:08 +0530

    added scrollspy

--- app/js/main.js
+++ app/js/main.js
@@ -26,10 +26,59 @@ function setupScrollreveal(){
 
 }
 
+
+function smoothScroll(){
+
+    const duration = 800;
+
+    $("#bs-example-navbar-collapse-2 ul li a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, duration, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+    });
+
+    $(".navbar-header a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+        if(!hash) return;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, duration, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+    });
+}
+
 $(document).ready(() =>{
     window.sr = ScrollReveal();
     runCountDownTimer(2017,8,6);
     handleScroll();
-    setupScrollreveal()
+    setupScrollreveal();
+    smoothScroll();
 
 });
