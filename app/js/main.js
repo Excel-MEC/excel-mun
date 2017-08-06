790da3296354976c883b227beacb46109ecc6f14
ashwinkk <ashwinkailas@gmail.com>
2017-08-06 19:53:34 +0530

    carousel changes

--- app/js/main.js
+++ app/js/main.js
@@ -1,105 +1,122 @@
-const countdownUnits = countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS;
+const countdownUnits =
+	countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS;
 
-function runCountDownTimer(year,month,day){
-    const date = new Date(year,month,day);
-    setInterval(()=>{
-        $('span.countdownTarget').html(countdown(date).toString() + " remaining.");
-    },1000);
+function runCountDownTimer(year, month, day) {
+	const date = new Date(year, month, day);
+	setInterval(() => {
+		$("span.countdownTarget").html(countdown(date).toString() + " remaining.");
+	}, 1000);
 }
 
-
-function handleScroll(){
-    $(document).scroll(() => {
-        if($(document).scrollTop() > 53){
-            $('nav').removeClass('navbar-transparent');
-        }else{
-            $('nav').addClass('navbar-transparent');
-        }
-    });
+function handleScroll() {
+	$(document).scroll(() => {
+		if ($(document).scrollTop() > 53) {
+			$("nav").removeClass("navbar-transparent");
+		} else {
+			$("nav").addClass("navbar-transparent");
+		}
+	});
 }
 
-function setupScrollreveal(){
-    sr.reveal('.marketing-element' ,{duration: 2000, origin: "bottom", distance: "200px"},60);
-    sr.reveal('.registration-alert' ,{duration: 500 ,origin: "top"});
-    sr.reveal('.about-us-heading' ,{duration: 1000 ,origin: "left"});
-    sr.reveal('.about-us-desc',{duration:1000,origin: "right"});
-    sr.reveal('.preparation-heading',{duration:1000,origin: "left"});
-    sr.reveal('.topic' ,{duration: 2000, origin: "bottom", distance: "20px"},60);
-    sr.reveal('.contact-no-el' ,{duration:1000,origin: "left"},60 );
-    sr.reveal('.contact-us-heading',{duration:1000,origin: "right"});
-    sr.reveal('.map-panel',{duration:1000,origin: "left"});
-    sr.reveal('.sponsor-img' ,{duration:1000,origin: "bottom"},60 );
-
-
+function setupScrollreveal() {
+	sr.reveal(
+		".marketing-element",
+		{ duration: 2000, origin: "bottom", distance: "200px" },
+		60
+	);
+	sr.reveal(".registration-alert", { duration: 500, origin: "top" });
+	sr.reveal(".about-us-heading", { duration: 1000, origin: "left" });
+	sr.reveal(".about-us-desc", { duration: 1000, origin: "right" });
+	sr.reveal(".preparation-heading", { duration: 1000, origin: "left" });
+	sr.reveal(
+		".topic",
+		{ duration: 2000, origin: "bottom", distance: "20px" },
+		60
+	);
+	sr.reveal(".contact-no-el", { duration: 1000, origin: "left" }, 60);
+	sr.reveal(".contact-us-heading", { duration: 1000, origin: "right" });
+	sr.reveal(".map-panel", { duration: 1000, origin: "left" });
+	sr.reveal(".sponsor-img", { duration: 1000, origin: "bottom" }, 60);
 }
 
+function smoothScroll() {
+	const duration = 800;
 
-function smoothScroll(){
+	$("#bs-example-navbar-collapse-2 ul li a[href^='#']").on("click", function(
+		e
+	) {
+		// prevent default anchor click behavior
+		e.preventDefault();
 
-    const duration = 800;
+		// store hash
+		var hash = this.hash;
 
-    $("#bs-example-navbar-collapse-2 ul li a[href^='#']").on('click', function(e) {
+		// animate
+		$("html, body").animate(
+			{
+				scrollTop: $(hash).offset().top
+			},
+			duration,
+			function() {
+				// when done, add hash to url
+				// (default click behaviour)
+				window.location.hash = hash;
+			}
+		);
+	});
 
-   // prevent default anchor click behavior
-   e.preventDefault();
+	$(".navbar-header a[href^='#']").on("click", function(e) {
+		// prevent default anchor click behavior
+		e.preventDefault();
 
-   // store hash
-   var hash = this.hash;
+		// store hash
+		var hash = this.hash;
 
-   // animate
-   $('html, body').animate({
-       scrollTop: $(hash).offset().top
-     }, duration, function(){
+		if (!hash) return;
 
-       // when done, add hash to url
-       // (default click behaviour)
-       window.location.hash = hash;
-     });
-
-    });
-
-    $(".navbar-header a[href^='#']").on('click', function(e) {
-
-   // prevent default anchor click behavior
-   e.preventDefault();
-
-   // store hash
-   var hash = this.hash;
-
-    if(!hash) return;
-
-   // animate
-   $('html, body').animate({
-       scrollTop: $(hash).offset().top
-     }, duration, function(){
-
-       // when done, add hash to url
-       // (default click behaviour)
-       window.location.hash = hash;
-     });
-
-    });
+		// animate
+		$("html, body").animate(
+			{
+				scrollTop: $(hash).offset().top
+			},
+			duration,
+			function() {
+				// when done, add hash to url
+				// (default click behaviour)
+				window.location.hash = hash;
+			}
+		);
+	});
 }
 
 function initMap() {
-    var myLocation = new google.maps.LatLng(9.9938,76.2906);
-    var mapOptions = {
-        center: myLocation,
-        zoom: 19
-    };
-    var marker = new google.maps.Marker({
-        position: myLocation,
-        title: "Property Location"
-    });
-    var map = new google.maps.Map(document.getElementById("map1"),
-        mapOptions);
-    marker.setMap(map);
+	var myLocation = new google.maps.LatLng(9.9938, 76.2906);
+	var mapOptions = {
+		center: myLocation,
+		zoom: 19
+	};
+	var marker = new google.maps.Marker({
+		position: myLocation,
+		title: "Property Location"
+	});
+	var map = new google.maps.Map(document.getElementById("map1"), mapOptions);
+	marker.setMap(map);
 }
 
-$(document).ready(() =>{
-    window.sr = ScrollReveal();
-    // runCountDownTimer(2017,9,29);
-    handleScroll();
-    setupScrollreveal();
-    smoothScroll();
+function carouselFill() {
+	var children = $(".item").children().each(function(child, element) {
+		console.log($(element).width());
+		if ($(element).width() > $(element).height())
+			$(".item>img").addClass("landscape");
+		else $(".item>img").addClass("portrait");
+	});
+}
+
+$(document).ready(() => {
+	window.sr = ScrollReveal();
+	carouselFill();
+	// runCountDownTimer(2017,9,29);
+	handleScroll();
+	setupScrollreveal();
+	smoothScroll();
 });
